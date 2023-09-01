88fc8ec25c86c05a7cce732d1243876132bbbe95
Zelans <qodirtok@gmail.com>
2023-09-01 08:41:52 +0700

    tes

---
+++ src/layouts/dashboard-custom/index.js
@@ -0,0 +1,34 @@
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Icon from "@mui/material/Icon";
+
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+
+// Soft UI Dashboard React examples
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+
+// Soft UI Dashboard React base styles
+import typography from "assets/theme/base/typography";
+
+// Dashboard layout components
+import BuildByDevelopers from "layouts/dashboard/components/BuildByDevelopers";
+import WorkWithTheRockets from "layouts/dashboard/components/WorkWithTheRockets";
+import Projects from "layouts/dashboard/components/Projects";
+import OrderOverview from "layouts/dashboard/components/OrderOverview";
+
+function dashboardcustom() {
+  const { size } = typography;
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <SoftBox py={3}></SoftBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default dashboardcustom;
